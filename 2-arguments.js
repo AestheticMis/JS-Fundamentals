eb4374f5c7b783fdf014930e5874aaa3d8bc3116
Emily <140704657+AestheticMis@users.noreply.github.com>
2025-07-09 16:37:33 +0200

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,1 +1,9 @@
+//Get the number of arguments passed, subtracting 2 for the node executable and script name
+const argsLength = process.argv.length -2;
 
+if (argsLength ===0){
+  console.log("No argument");
+}else if(argsLength ===1){
+  console.log("Argument found");
+}elseconsole.log("Arguments found");
+}
