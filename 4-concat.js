736d6009d43e0f0a7edeb354957591babe2c94c4
Emily <140704657+AestheticMis@users.noreply.github.com>
2025-07-11 16:22:50 +0200

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,3 +1,8 @@
+<?php
+<first argument> is <second argument>
 
-let myArray= ["c","Cool","Python","fun"];
-console.log('${process.argv[2]} is ${process.argv[3]}');
+?>
+  const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(`${arg1} is ${arg2}`);
