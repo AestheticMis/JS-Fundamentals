088d2e7f7d3d82aea2d9d9ff26519ae80adc3705
Emily <140704657+AestheticMis@users.noreply.github.com>
2025-07-11 16:23:55 +0200

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,8 +1,5 @@
-<?php
-<first argument> is <second argument>
 
-?>
   const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-console.log(`${arg1} is ${arg2}`);
+console.log(`${Python} is ${fun}`);
