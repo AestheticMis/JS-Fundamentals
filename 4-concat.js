c5654829bc2d0b24c12f3ddbbb13496f253ac075
Emily <140704657+AestheticMis@users.noreply.github.com>
2025-07-15 16:41:12 +0200

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 const arg1 = args[0] !== undefined ? args[0] : "";
 const arg2 = args[1] !== undefined ? args[1] : "";
 
-console.log(`${C} is ${Cool}`);
+console.log(`${Python} is ${Fun}`);
 
 
 
