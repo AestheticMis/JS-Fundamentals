ec98ec4e20cbe959ce9159b561ca1d680556b026
Emily <140704657+AestheticMis@users.noreply.github.com>
2025-07-15 16:53:52 +0200

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,16 +1,15 @@
+
+
 const args = process.argv.slice(2);
 
-const firstArg = args[0];
-const secondArg = args[1];
+const firstArg = args[0] === undefined ? "undefined" : args[0];
+const secondArg = args[1] === undefined ? "undefined" : args[1];
 
-if (args.length === 2) {
+if (args.length === 1) {
+  console.log(`HBTN is undefined`);
+} else {
   console.log(`${Python} is ${Fun}`);
-} else if (args.length === 1) {
-  console.log("HBTN");
 }
-// No output for 0 arguments
-
-
 
 
   
