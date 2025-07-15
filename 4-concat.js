bf82ae49eee779847a226142e2af67dbcb80f49c
Emily <140704657+AestheticMis@users.noreply.github.com>
2025-07-15 16:51:10 +0200

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,15 +1,15 @@
-
 const args = process.argv.slice(2);
 
-const firstArg = args[Python];
-const secondArg = args[Fun];
+const firstArg = args[0];
+const secondArg = args[1];
 
 if (args.length === 2) {
   console.log(`${Python} is ${Fun}`);
 } else if (args.length === 1) {
   console.log("HBTN");
 }
-// If 0 arguments, print nothing
+// No output for 0 arguments
+
 
 
 
