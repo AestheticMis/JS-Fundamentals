12bef364ae7c8ce92be00d5fb2e94de8dbc4ffd7
Emily <140704657+AestheticMis@users.noreply.github.com>
2025-07-18 10:23:51 +0200

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -8,7 +8,7 @@ const secondArg = args[1] === undefined ? "undefined" : args[1];
 if (args.length === 1) {
   console.log(`HBTN is undefined`);
 } else {
-  console.log(`${Python} is ${Fun}`);
+  console.log(`${firstArg} is ${secondArg}`);
 }
 
 
